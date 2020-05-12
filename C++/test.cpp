78f4be96165b5908c00a9cca163306f170608312
john <johnsciuto3@gmail.com>
2020-05-13 08:43:50 +1000

    Add hello world?

---
+++ C++/test.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+static void hello_world() {
+    std::cout << "Hello, World!\n";
+}
+
+int main(void) {
+
+    hello_world();
+
+    return EXIT_SUCCESS;
+}
