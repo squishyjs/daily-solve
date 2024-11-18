301f54746b2109ed8faa6590183223be2eec775a
john <johnsciuto3@gmail.com>
2024-11-18 22:01:25 +1100

    Learning Pointer to Arrays

--- C++/Arrays.cpp
+++ C++/Arrays.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
-
 int main() {
 
+    int my_array[] = {1, 2, 3, 4};
 
-    return 0;;
+    // Define a pointer
+    int* ptr = my_array;
+
+    // Printing address of my_array using array name
+    std::cout << "Memory address of my array is: " << &my_array << std::endl;
+    
+    // Printing address of the array using pointer
+    std::cout << "Memory address of my array is: " << ptr << std::endl;
+
+    return 0;
 }
