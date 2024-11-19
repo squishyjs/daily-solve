87354a889c7a8d2d97d1ccf311dd663b15d94ba1
john <johnsciuto3@gmail.com>
2024-11-19 20:36:41 +1100

    Updated Arrays.cpp

--- C++/Arrays.cpp
+++ C++/Arrays.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 
-int main() {
+void array_address_pointer() {
 
     int my_array[] = {1, 2, 3, 4};
-
+    
     // Define a pointer
     int* ptr = my_array;
-
+    
     // Printing address of my_array using array name
     std::cout << "Memory address of my array is: " << &my_array << std::endl;
-    
+
     // Printing address of the array using pointer
     std::cout << "Memory address of my array is: " << ptr << std::endl;
+}
 
+void array_pointers() {
+    std::cout << "hello world" << std::endl;
+}
+
+int main() {
+
+    array_pointers(); // call the function -> hello world
+
+    array_address_pointer(); // 
     return 0;
 }
