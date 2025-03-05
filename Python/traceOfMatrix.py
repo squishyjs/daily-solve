0fb3b6618e75e19215caa378b91ea22863832260
john <johnsciuto3@gmail.com>
2025-03-05 12:37:01 +1100

    Numpy Alternative Solution Added

--- Python/traceOfMatrix.py
+++ Python/traceOfMatrix.py
@@ -1,8 +1,21 @@
+import numpy as np
 from typing import List
     
 
 def compute_trace_matrix(matrix: List[int]):
     return [1]
+
+def solution_two(matrix, matrix_size: int):
+    matrix = np.array(matrix)
+    n = matrix_size
+
+    diagonal_sums = [matrix.trace()]
+    for i in range(1, n):
+        diagonal_sums.append(matrix[0:n-i, i:n])
+        diagonal_sums.append(matrix[i:n, 0:n-i])
+
+    return max(diagonal_sums)
+
 
 def main() -> None:
     test_cases = int(input())
@@ -20,11 +33,11 @@ def main() -> None:
                 temp += matrix[j + i][j]
             # udpate result with temp if larger
             result = max(temp, result)
-        for i in range(1, n):
+        for j in range(1, n):
             temp: int = 0
             for i in range(n - j):
                 temp += matrix[i][j + i]
-            result = max(temp, result)
+            result = max(temp, result) 
         print(result)
         
 
