c906febc11860ac4dfd8f55cd1ec01f357f2cca6
john <johnsciuto3@gmail.com>
2025-03-09 09:32:54 +1100

    Solved Single Use Attack

---
+++ C++/SingleUseAttack.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main() {
+
+    int testCases;
+    std::cin >> testCases;
+    while (testCases--) {
+        int h, x, y;
+        std::cin >> h >> x >> y;
+        int attacks;
+        if (y >= h) {
+            attacks = 1;
+            std::cout << attacks << std::endl;
+            continue;
+        }
+        attacks = ceil((h - y) / (x * 1.0)) + 1;
+        std::cout << attacks << std::endl;
+    }
+    return 0;
+}
