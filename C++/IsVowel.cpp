2f39597e9a2d5a36f5e496768f89b1eb46ede44d
john <johnsciuto3@gmail.com>
2025-04-16 09:47:52 +1000

    Solved IsVowel

---
+++ C++/IsVowel.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+bool isVowel(char &specificCharacter) {
+    std::vector<char> vowels = {'A', 'E', 'I', 'O', 'U'};
+    for (int i = 0; i < 5; ++i) {
+        char currChar = vowels[i];
+        if (specificCharacter == currChar) {
+            return true;
+        }
+    }
+    return false;
+}
+//main function
+int main() {
+    int testCases;
+    std::cin >> testCases;
+    while (testCases--) {
+        char ch;
+        std::cin >> ch;
+        bool result = isVowel(ch);
+        if (result) {
+            std::cout << "Vowel\n";
+        } else {
+            std::cout << "Consonant\n";
+        }
+    }
+    return 0;
+}
