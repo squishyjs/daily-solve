6a51faac7137e64a47ba19a0350b31205a7991e6
john <johnsciuto3@gmail.com>
2025-04-15 14:41:51 +1000

    Solving Smallest Notes

---
+++ C++/SmallestNotes.cpp
@@ -0,0 +1,51 @@
+/* Consider a currency system in which there are notes of six denominations, namely, Rs. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100.
+If the sum of Rs. N is input, write a program to compute smallest number of notes that will combine to give Rs. N.
+
+Input
+
+The first line contains an integer T, total number of testcases. Then follow T lines, each line contains an integer N.
+
+Output
+For each test case, display the smallest number of notes that will combine to give N, in a new line. 
+
+Example:
+Input 1200 -> Output: 12
+Input 500  -> Output: 5
+Input 247  -> Output: 7
+*/
+#include <ios>
+#include <iostream>
+
+int solveSmallestNotes(int &rupees) {
+    if (rupees == 1) {
+        return 1;
+    }
+    std::vector<int> table {100, 50, 10, 5, 2, 1};
+    int operations = 0;
+    int currIndex = 0;
+    while (rupees > 0) {
+        //cook your dish here
+        if (rupees - table[currIndex] > 0) {
+            //some code that counts the amount of operations before moving to the next one
+            int denomination = table[currIndex];
+            operations += (rupees / denomination);
+            //update rupees to smaller value
+            rupees = rupees % denomination;
+        } else {
+            currIndex++;
+        }
+    }
+    return operations;
+}
+
+int main() {
+    int testCases;
+    std::cin >> testCases;
+    while (testCases--) {
+        int rupees;
+        std::cin >> rupees;
+        std::cout << solveSmallestNotes(rupees);
+        std::cout << std::endl;
+    }
+    return 0;
+}
