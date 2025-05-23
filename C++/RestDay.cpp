0b3029a1c010743ead575b46150725d4358adc37
john <johnsciuto3@gmail.com>
2025-05-23 10:11:51 +1000

    Solved Rest Day

---
+++ C++/RestDay.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#define F  fflush(stdout)
+int main(int argument_count, char* argument_vector[]) {
+    int n; std::cin >> n;
+    printf("%d", n * 3); F;
+    return EXIT_SUCCESS;
+}
