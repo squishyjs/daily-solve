5bba412eff07e93bac72d249a0773586d7ac0bcf
john <johnsciuto3@gmail.com>
2025-11-09 10:40:18 +1100

    Solved Wolf Down

---
+++ C++/WolfDown.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+#define WOLF '1'
+#define BIRD '0'
+
+
+int solve(int n, std::string s) {
+
+    int safe = 0;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == WOLF) {
+            break;
+        }
+
+        if (s[i] == BIRD) {
+            safe++;
+        }
+    }
+
+    return safe;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(NULL);
+    std::cin.tie(NULL);
+
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int n;
+        std::cin >> n;
+        std::string s;
+        std::cin >> s;
+
+        int res = solve(n, s);
+        std::cout << res << std::endl;
+    }
+
+    return 0;
+}
