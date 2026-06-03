6b53f9ea3ec8399c62b1e5053feb57aecf4e4ca4
john <johnsciuto3@gmail.com>
2026-06-04 09:43:37 +1000

    Solved Chef and Operators

---
+++ C++/ChefandOperators.cpp
@@ -0,0 +1,39 @@
+#include <cstdint>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using u32 = uint_fast32_t;
+using u64 = uint_fast64_t;
+
+static char solve(const int a, const int b_) {
+    if (a < b_) {
+        return '>';
+    }
+
+    if (a > b_) {
+        return '<';
+    }
+
+    return '=';
+}
+
+int main(void) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int a, b;
+        std::cin >> a >> b;
+
+        std::cout << solve(a, b);
+        std::cout << "\n";
+    }
+
+    return 0;
+}
